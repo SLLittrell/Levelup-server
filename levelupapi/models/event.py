7dbfb4f8a84a5623c4c13f8c5e696e67a5cec2a3
sllittrell <stacey.littrell@gmail.com>
2021-05-04 10:02:51 -0500

    seeded data for games, events, and gamers

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
-    date = models.DateField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     host = models.ForeignKey("Gamer", on_delete=models.CASCADE)
