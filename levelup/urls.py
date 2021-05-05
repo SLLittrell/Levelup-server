d2635fdefbb7e6e159b501d312f361c4efc00528
sllittrell <stacey.littrell@gmail.com>
2021-05-05 14:54:46 -0500

    Created game views and serializer and connected the routse

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,10 +17,11 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameCategories
+from levelupapi.views import GameCategories, Games
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gamecategories', GameCategories, 'gamecategory')
+router.register(r'games', Games, 'game')
 
 
 urlpatterns = [
