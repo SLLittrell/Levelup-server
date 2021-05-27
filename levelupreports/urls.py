eb7a6ee41dc67b86bc03d624862dc1f2711266bb
sllittrell <stacey.littrell@gmail.com>
2021-05-27 10:42:14 -0500

    Event Report renders in html format on DOM

--- levelupreports/urls.py
+++ levelupreports/urls.py
@@ -1,6 +1,8 @@
+from levelupreports.views.users.eventsbyuser import userevents_list
 from django.urls import path
 from .views import usergame_list
 
 urlpatterns = [
     path('reports/usergames', usergame_list),
+    path('reports/userevents', userevents_list),
 ]
